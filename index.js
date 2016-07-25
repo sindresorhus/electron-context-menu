bf3096ede85b614b7ca5aa062ddcb5773d61dca6
Sergey <sergey.nazarov@galacticexchange.io>
2016-07-25 13:30:02 +0300

    Changed var to let

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function create(win, opts) {
 function deleteUnnecessarySeparators(menuTpl) {
 	let visiblePreviousEl;
 	return menuTpl.filter((el, i, arr) => {
-		var toDelete = el.type === 'separator' && (!visiblePreviousEl || i === arr.length - 1 || arr[i + 1].type === 'separator');
+		let toDelete = el.type === 'separator' && (!visiblePreviousEl || i === arr.length - 1 || arr[i + 1].type === 'separator');
 		visiblePreviousEl = toDelete || !isVisible(el) ? visiblePreviousEl : el;
 		return !toDelete;
 	});
