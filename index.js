a80a18e2a4a424312eee34dc8ee63112cdca8e12
Sergey <sergey.nazarov@galacticexchange.io>
2016-07-25 13:17:32 +0300

    Delete separators when previous elemnts are not visible and doesn't show poup menu when no visible elemnts are available

--- index.js
+++ index.js
@@ -89,11 +89,26 @@ function create(win, opts) {
 
 		// filter out leading/trailing separators
 		// TODO: https://github.com/electron/electron/issues/5869
-		menuTpl = menuTpl.filter((el, i, arr) => !(el.type === 'separator' && (i === 0 || i === arr.length - 1 || arr[i + 1].type === 'separator')));
+		menuTpl = deleteUnnecessarySeparators(menuTpl);
 
-		const menu = (electron.Menu || electron.remote.Menu).buildFromTemplate(menuTpl);
-		menu.popup(win);
+		if (menuTpl.some(elem => isVisible(elem))) {
+			const menu = (electron.Menu || electron.remote.Menu).buildFromTemplate(menuTpl);
+			menu.popup(win);
+		}
 	});
+}
+
+function deleteUnnecessarySeparators(menuTpl) {
+	let visiblePreviousEl;
+	return menuTpl.filter((el, i, arr) => {
+		var toDelete = el.type === 'separator' && (!visiblePreviousEl || i === arr.length - 1 || arr[i + 1].type === 'separator');
+		visiblePreviousEl = toDelete || !isVisible(el) ? visiblePreviousEl : el;
+		return !toDelete;
+	});
+}
+
+function isVisible(elem) {
+	return !('visible' in elem) || elem.visible;
 }
 
 module.exports = (opts = {}) => {
