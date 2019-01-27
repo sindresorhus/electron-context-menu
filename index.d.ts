a79e673278f3bd349e246ab3cddb95eef12c7850
Jarek Radosz <jradosz@gmail.com>
2019-01-27 19:21:41 +0100

    Convert spaces to tabs

--- index.d.ts
+++ index.d.ts
@@ -1,100 +1,100 @@
 export interface Labels {
-  /**
-   * @default 'Cut'
-   */
-  cut?: string;
+	/**
+	 * @default 'Cut'
+	 */
+	cut?: string;
 
-  /**
-   * @default 'Copy'
-   */
-  copy?: string;
+	/**
+	 * @default 'Copy'
+	 */
+	copy?: string;
 
-  /**
-   * @default 'Paste'
-   */
-  paste?: string;
+	/**
+	 * @default 'Paste'
+	 */
+	paste?: string;
 
-  /**
-   * @default 'Save Image'
-   */
-  save?: string;
+	/**
+	 * @default 'Save Image'
+	 */
+	save?: string;
 
-  /**
-   * @default 'Save Image As…'
-   */
-  saveImageAs?: string;
+	/**
+	 * @default 'Save Image As…'
+	 */
+	saveImageAs?: string;
 
-  /**
-   * @default 'Copy Link'
-   */
-  copyLink?: string;
+	/**
+	 * @default 'Copy Link'
+	 */
+	copyLink?: string;
 
-  /**
-   * @default 'Copy Image Address'
-   */
-  copyImageAddress?: string;
+	/**
+	 * @default 'Copy Image Address'
+	 */
+	copyImageAddress?: string;
 
-  /**
-   * @default 'Inspect Element'
-   */
-  inspect?: string;
+	/**
+	 * @default 'Inspect Element'
+	 */
+	inspect?: string;
 }
 
 export interface Options {
-  /**
-   * Window or WebView to add the context menu to.
-   * When not specified, the context menu will be added to all existing and new windows.
-   */
-  window?: Electron.BrowserWindow | Electron.WebviewTag;
+	/**
+	 * Window or WebView to add the context menu to.
+	 * When not specified, the context menu will be added to all existing and new windows.
+	 */
+	window?: Electron.BrowserWindow | Electron.WebviewTag;
 
-  /**
-   * Should return an array of [menu items](https://electronjs.org/docs/api/menu-item) to be prepended to the context menu.
-   */
-  prepend?: (params: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
+	/**
+	 * Should return an array of [menu items](https://electronjs.org/docs/api/menu-item) to be prepended to the context menu.
+	 */
+	prepend?: (params: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
 
-  /**
-   * Should return an array of [menu items](https://electronjs.org/docs/api/menu-item) to be appended to the context menu.
-   */
-  append?: (param: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
+	/**
+	 * Should return an array of [menu items](https://electronjs.org/docs/api/menu-item) to be appended to the context menu.
+	 */
+	append?: (param: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
 
-  /**
-   * Show the `Copy Image Address` menu item when right-clicking on an image.
-   *
-   * @default false
-   */
-  showCopyImageAddress?: boolean;
+	/**
+	 * Show the `Copy Image Address` menu item when right-clicking on an image.
+	 *
+	 * @default false
+	 */
+	showCopyImageAddress?: boolean;
 
-  /**
-   * Show the `Save Image As…` menu item when right-clicking on an image.
-   *
-   * @default false
-   */
-  showSaveImageAs?: boolean;
+	/**
+	 * Show the `Save Image As…` menu item when right-clicking on an image.
+	 *
+	 * @default false
+	 */
+	showSaveImageAs?: boolean;
 
-  /**
-   * Force enable or disable the `Inspect Element` menu item.
-   *
-   * Default: [Only in development](https://github.com/sindresorhus/electron-is-dev)
-   */
-  showInspectElement?: boolean;
+	/**
+	 * Force enable or disable the `Inspect Element` menu item.
+	 *
+	 * Default: [Only in development](https://github.com/sindresorhus/electron-is-dev)
+	 */
+	showInspectElement?: boolean;
 
-  /**
-   * Overwrite labels for the default menu items. Useful for i18n.
-   *
-   * @default {}
-   */
-  labels?: Labels;
+	/**
+	 * Overwrite labels for the default menu items. Useful for i18n.
+	 *
+	 * @default {}
+	 */
+	labels?: Labels;
 
-  /**
-   * Determines whether or not to show the menu.
-   * Can be useful if you for example have other code presenting a context menu in some contexts.
-   *
-   * @example
-   *
-   * // Doesn't show the menu if the element is editable
-   * shouldShowMenu: (event, params) => !params.isEditable
-   */
-  shouldShowMenu?: (event: Electron.Event, params: Electron.ContextMenuParams) => boolean;
+	/**
+	 * Determines whether or not to show the menu.
+	 * Can be useful if you for example have other code presenting a context menu in some contexts.
+	 *
+	 * @example
+	 *
+	 * // Doesn't show the menu if the element is editable
+	 * shouldShowMenu: (event, params) => !params.isEditable
+	 */
+	shouldShowMenu?: (event: Electron.Event, params: Electron.ContextMenuParams) => boolean;
 }
 
 /**
