46eae939643585bbbb9a5f6c2ffa7b4bf0dd3bfe
Sindre Sorhus <sindresorhus@gmail.com>
2019-01-28 01:03:11 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -13,7 +13,7 @@ export interface Options {
   /**
    * Should return an array of [menu items](https://electronjs.org/docs/api/menu-item) to be appended to the context menu.
    */
-  append?: (params: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
+  append?: (param: Electron.ContextMenuParams, browserWindow: Electron.BrowserWindow | Electron.WebviewTag) => Electron.MenuItem[];
 
   /**
    * Show the `Copy Image Address` menu item when right-clicking on an image.
@@ -38,6 +38,8 @@ export interface Options {
 
   /**
    * Overwrite labels for the default menu items. Useful for i18n.
+	 *
+	 * @default {}
    *
    * @example
    *
@@ -51,8 +53,6 @@ export interface Options {
    * 	copyImageAddress: 'Configured Copy Image Address',
    * 	inspect: 'Configured Inspect'
    * }
-   *
-   * @default {}
    */
   labels: object;
 
@@ -70,9 +70,8 @@ export interface Options {
 
 /**
  * This module gives you a nice extensible context menu with items like `Cut`/`Copy`/`Paste` for text, `Save Image` for images, and `Copy Link` for links. It also adds an `Inspect Element` menu item when in development to quickly view items in the inspector like in Chrome.
- * You can use this module directly in both the main and renderer process.
  *
- * @param options
+ * You can use this module directly in both the main and renderer process.
  *
  * @example
  *
@@ -87,9 +86,10 @@ export interface Options {
  * 	}]
  * });
  *
- * let mainWindow;
- * app.on('ready', () => {
- * 	mainWindow = new BrowserWindow();
+ * let win;
+ * (async () => {
+ * 	await app.whenReady();
+ * 	win = new BrowserWindow();
  * });
  */
 export default function contextMenu(options?: Options): void;
