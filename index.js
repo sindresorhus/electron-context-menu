88bc07ff5c3166b96481556c290517fd85bc7850
gaodeng <gaodengming@gmail.com>
2019-05-30 20:12:57 +0800

    dynamic labels

--- index.js
+++ index.js
@@ -184,9 +184,13 @@ const create = (win, options) => {
 
 		// Apply custom labels for default menu items
 		if (options.labels) {
+			let labels=options.labels;
+			if(typeof options.labels === 'function'){
+				labels=options.labels();
+			}
 			for (const menuItem of menuTemplate) {
-				if (options.labels[menuItem.id]) {
-					menuItem.label = options.labels[menuItem.id];
+				if (labels[menuItem.id]) {
+					menuItem.label = labels[menuItem.id];
 				}
 			}
 		}
